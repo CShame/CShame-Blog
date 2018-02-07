ea7effde78836f6f93e83607a08263dea6404580
CShame <1138158134@qq.com>
2018-02-07 17:14:54 +0800

    移出多余的index.html

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 module.exports = function (app) {
   app.get('/', function (req, res) {
-    res.redirect('/index');
+    res.redirect('/posts');
   });
   app.use('/signup', require('./signup'));
   app.use('/signin', require('./signin'));
   app.use('/signout', require('./signout'));
   app.use('/posts', require('./posts'));
-  app.use('/index', require('./indexpage'));
+  // app.use('/index', require('./indexpage'));
 
   // 404 page
   app.use(function (req, res) {
